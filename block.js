9942c4950201ee4d652099a9970d264295ece34d
gsfordham <rhoblock3@outlook.com>
2018-08-30 21:59:42 -0400

    Main file

---
+++ block.js
@@ -0,0 +1,3 @@
+//Add as a bookmark
+
+javascript: (function() {setInterval(function(){document.querySelectorAll("div[class^=messageGroupBlocked]").forEach((x)=>{x.style.display = "none";})}, 2000)})();
